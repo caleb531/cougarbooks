36aabc869a4ffb0caf9e243c542b4f991cabaa8b
evancausey <Evan@tlpartners.com>
2016-11-10 18:17:20 -0800

    Update db-select.sql
    Just a few kinda fun to see what we can come up with.
    The first select is an example of loading the main page with adds that are currently open.
    The second select is an example of looking up books sold by a particular user.
    The third select is an example of looking up books that contain the word "new" is their description.
    The fourth select is an example of looking up books that contain the word "computer" is their title.
    
    
    TODO: add check "if book is already sold, do not show it".

---
+++ misc/db-select.sql
@@ -0,0 +1,17 @@
+Select book_title, book_author, book_isbn, listed_price, ad_description
+FROM ad
+WHERE ad.is_closed = 1;
+
+Select book_title, book_author, book_isbn, listed_price, ad_description
+FROM ad
+WHERE ad.user_id = (select DISTINCT user_id
+From user
+WHERE user.email_address  = "EC@cougars.csusm.edu");
+
+SELECT book_title, book_author, book_isbn, listed_price, ad_description
+FROM ad 
+WHERE ad_description LIKE '%new%';
+
+SELECT book_title, book_author, book_isbn, listed_price, ad_description
+FROM ad 
+WHERE book_title LIKE '%science%';
