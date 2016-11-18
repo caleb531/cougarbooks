0ba6b341b600f5a9229046c35c4f0b6c3de6ef14
Caleb Evans <caleb@calebevans.me>
2016-11-18 09:57:20 -0800

    Fix line endings in database files (should be LF)

--- database/db-select.sql
+++ database/db-select.sql
@@ -9,9 +9,9 @@ From user
 WHERE user.email_address  = "EC@cougars.csusm.edu");
 
 SELECT book_title, book_author, book_isbn, listed_price, ad_description
-FROM ad 
+FROM ad
 WHERE ad_description LIKE '%new%';
 
 SELECT book_title, book_author, book_isbn, listed_price, ad_description
-FROM ad 
+FROM ad
 WHERE book_title LIKE '%science%';
