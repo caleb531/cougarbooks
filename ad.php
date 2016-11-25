751f552e12f3a3b9e5a734d6d813ed0acaa8cba0
Moreno <moren081@cougars.csusm.edu>
2016-11-24 17:52:41 -0800

    ad.php - retrieves information from database

--- ad.php
+++ ad.php
@@ -12,21 +12,54 @@ include('assets/php/head.php');
 
 	<main>
 
-		<div class="book-ad">
-			<img class="book-image" src="http://akamaicovers.oreilly.com/images/9780596158118/cat.gif" alt="python-book" />
-			<a href="post.php?ad=1" class="edit-ad-link">Edit</a>
-			<h1 class="book-title">Programming Python</h1>
-			<div class="book-authors"><span class="book-attr-label">Author(s):</span> Mary Ann</div>
-			<div class="book-edition"><span class="book-attr-label">Edition:</span> 5</div>
-			<div class="isbn"><span class="book-attr-label">ISBN:</span> 978-3-16-148410-0</div>
-			<div class="book-price"><span class="book-attr-label">Price:</span> $50.00</div>
-			<p class="book-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut...</p>
+
+		<?php
+					$ad_id = trim($_GET['ad']);
+		      $sql = "SELECT * FROM ad where ad_id LIKE '$ad_id'";
+					$ads = $db->fetchAll( $sql, array());
+		?>
+
+		<div class="book-ad<?php echo $ad['is_closed'] ? ' closed' : ''; ?>">
+			<?php foreach ( $ads as $ad ): ?>
+
+				<?php if ( $ad['is_closed'] ): ?>
+					<span class="closed-badge">Closed</span>
+				<?php endif; ?>
+
+				<?php if ( ! empty( $ad['path_to_picture'] ) ): ?>
+					<?php display_ad_image( $ad['ad_id'] ); ?>
+				<?php endif; ?>
+
+				<?php if($_SESSION['signed_in']): ?>
+						<?php if ( $db->allowed_to_edit_ad( $_GET['ad'] ) ): ?>
+							<a href="post.php" class="edit-ad-link">Edit</a>
+						<?php endif ?>
+				<?php endif ?>
+				<h1 class="book-title"><?php echo htmlspecialchars( $ad['book_title'] ); ?></a></h1>
+				<div class="book-authors"><span class="book-attr-label">Author(s):</span> <?php echo htmlspecialchars( $ad['book_author'] ); ?></div>
+				<div class="book-edition"><span class="book-attr-label">Edition:</span> <?php echo htmlspecialchars( $ad['book_edition'] ); ?></div>
+				<div class="book-isbn"><span class="book-attr-label">ISBN:</span> <?php echo htmlspecialchars( $ad['book_isbn'] ); ?></div>
+				<div class="book-price"><span class="book-attr-label">Price:</span> $<?php echo htmlspecialchars( $ad['listed_price'] ); ?></div>
+				<div class="ad-post-date"><span class="book-attr-label">Posted:</span> <?php echo strftime( '%b %d, %Y @ %-I:%M %p', strtotime( $ad['creation_time'] ) ); ?></div>
+
+				<?php $userID = $ad['user_id'];
+					$query = "SELECT * FROM user WHERE user_id LIKE '$userID'";
+					$users = $db->fetchAll( $query, array() );
+				?>
+
+				<div class="clearfix"></div>
+				<hr>
+					<?php foreach ($users as $user): ?>
+						<div class="contact-info">To buy this textbook, contact:</div>
+							<div class="firt-name"><span class="first-name">First Name:</span> <?php echo htmlspecialchars( $user['first_name'] ); ?></div>
+							<div class="last-name"><span class="last-name">Last Name:</span> <?php echo htmlspecialchars( $user['last_name'] ); ?></div>
+						  <div class="email-address"><span class="email-address">Email Address:</span> 	<?php echo htmlspecialchars( $user['email_address'] ); ?></div>
+							<div class="phone-number"><span class="phone-number">Phone No.:</span> <?php echo htmlspecialchars( $user['telephone_no'] ); ?></div>
+						</div>
+					<?php endforeach; ?>
+
+				<?php endforeach; ?>
 		</div>
-
-		<div class="clearfix"></div>
-		<hr>
-
-		<div class="contact-info">To buy this textbook, contact John Smith at 314-159-2635 (<a href="mailto:johnsmith@example.com">email</a>)</div>
 
 	</main>
 	<?php
