08e8017beda12065444a53af15eb6e5c164521b3
Caleb Evans <caleb@calebevans.me>
2016-11-29 14:56:10 -0800

    Clean up code for Account UI

--- account.php
+++ account.php
@@ -11,39 +11,51 @@ include('assets/php/head.php');
 
 	<main>
 
+		<?php if ( isset( $_GET['fail'] ) ): ?>
+			<div class="notification warning">Entered passwords do not match. Please try again.</div>
+		<?php endif; ?>
+
 		<h1>Account Info</h1>
+
 		<?php if ( isset( $_SESSION['signed_in'] ) ): ?>
-		  <?php
-		    $query = 'SELECT first_name, last_name, email_address, telephone_no FROM user WHERE user_id = :user_id';
-		    $row = $db->fetchOne( $query, array(
-		      'user_id' => $_SESSION['user_id']
-		    ) );
-		  ?>
-		<form class="account-info-form" action="assets/php/account.php" method="post">
 
-			<label for="first-name-field">First Name:</label><br>
-			<input id="first-name-field" type="text" name="first_name" value="<?php echo $row['first_name'] ?>" required maxlength="30" autofocus><br>
+			<?php
+			$query = 'SELECT first_name, last_name, email_address, telephone_no FROM user WHERE user_id = :user_id';
+			$user = $db->fetchOne( $query, array(
+				'user_id' => $_SESSION['user_id']
+			) );
+			?>
+			<form class="account-info-form" action="assets/php/account.php" method="post">
 
-			<label for="last-name-field">Last Name:</label><br>
-			<input id="last-name-field" type="text" name="last_name" value="<?php echo $row['last_name'] ?>" required maxlength="30"><br>
+				<label for="first-name-field">First Name:</label><br>
+				<input id="first-name-field" type="text" name="first_name" value="<?php echo htmlspecialchars( $user['first_name'] ); ?>" required maxlength="30" autofocus><br>
 
-			<label for="email-field">Email:</label><br>
-			<input id="email-field" type="text" name="email" value="<?php echo $row['email_address'] ?>" required placeholder="me@example.com" pattern="\s*([^@]+)@([^@]+)\s*"><br>
+				<label for="last-name-field">Last Name:</label><br>
+				<input id="last-name-field" type="text" name="last_name" value="<?php echo htmlspecialchars( $user['last_name'] ); ?>" required maxlength="30"><br>
 
-			<label for="phone-field">Phone Number:</label><br>
-			<input id="phone-field" type="text" name="phone" value="<?php echo $row['telephone_no'] ?>" placeholder="xxx-xxx-xxxx" pattern="\s*(\d{3})-?(\d{3})-?(\d{4})\s*"><br>
+				<label for="email-field">Email:</label><br>
+				<input id="email-field" type="text" name="email" value="<?php echo htmlspecialchars( $user['email_address'] ); ?>" required placeholder="me@example.com" pattern="\s*([^@]+)@([^@]+)\s*"><br>
 
-			<label for="new-password-field">Enter Password:</label><br>
-			<input id="new-password-field" type="password" name="new_password" required><br>
+				<label for="phone-field">Phone Number:</label><br>
+				<input id="phone-field" type="text" name="phone" value="<?php echo htmlspecialchars( $user['telephone_no'] ); ?>" placeholder="xxx-xxx-xxxx" pattern="\s*(\d{3})-?(\d{3})-?(\d{4})\s*"><br>
 
-			<label for="confirm-password-field">Confirm Password:</label><br>
-			<input id="confirm-password-field" type="password" name="confirm_password" required><br>
-			<button type="submit" name="submit">Save Changes</button>
-		</form>
+				<label for="password-field">Enter Password:</label><br>
+				<input id="password-field" type="password" name="password" required><br>
+
+				<label for="confirm-password-field">Confirm Password:</label><br>
+				<input id="confirm-password-field" type="password" name="confirm_password" required><br>
+				<button type="submit" name="submit">Save Changes</button>
+			</form>
+
+		<?php else: ?>
+
+			<p>You must <a href="<?php echo cb_get_signin_url(); ?>">sign in</a> or <a href="<?php echo cb_get_register_url(); ?>">register</a> to access this page.</p>
+
+		<?php endif; ?>
+
 	</main>
 	<?php
 	include('assets/php/footer.php');
 	?>
 </body>
 </html>
-<?php endif; ?>
--- assets/php/account.php
+++ assets/php/account.php
@@ -1,16 +1,54 @@
 <?php
-
 session_start();
 include('database.php');
 include('redirection.php');
 
-  $first = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
-  $last= filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
-  $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-  $phone= filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
-  $pass_word= filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-  $hash_password = $db->hash_password($pass_word,$email);
-  $update_query='UPDATE user SET first_name=:first_name, last_name=:last_name, email_address=:email_address, telephone_no=:phone_no, user_password=:hashed_password WHERE user_id = :user_id';
-  $db->query($update_query, array(':first_name' => $first,':last_name' => $last,':email_address' => $email,':phone_no' => $phone,':hashed_password' => $hash_password, 'user_id' => $_SESSION['user_id']));
-  cb_redirect('../../account.php');
+// Format the phone number by removing dashes
+function cb_format_telephone_no( $telephone_no ) {
+	return trim( str_replace( '-', '', $telephone_no ) );
+}
+
+$first_name = trim( $_POST['first_name'] );
+$last_name = trim( $_POST['last_name'] );
+$email_address = trim( $_POST['email'] );
+if ( ! empty( $_POST['phone'] ) ) {
+	$telephone_no = cb_format_telephone_no( $_POST['phone'] );
+} else {
+	$telephone_no = null;
+}
+
+$password = $_POST['password'];
+$confirmed_password = $_POST['confirm_password'];
+$hashed_password = $db->hash_password( $password, $email_address );
+
+// Only save changes in entered passwords match
+if ( $password === $confirmed_password ) {
+
+	$query = 'UPDATE user SET
+		first_name = :first_name,
+		last_name = :last_name,
+		email_address = :email_address,
+		telephone_no = :telephone_no,
+		user_password = :hashed_password
+		WHERE user_id = :user_id';
+
+	$db->query( $query, array(
+		'first_name' => $first_name,
+		'last_name' => $last_name,
+		'email_address' => $email_address,
+		'telephone_no' => $telephone_no,
+		'hashed_password' => $hashed_password,
+		'user_id' => $_SESSION['user_id']
+	) );
+
+	// Redirect back to Account page after saving changes
+	cb_redirect('../../account.php');
+
+} else {
+
+	// Return to Account page indicating that entered passwords do not match
+	cb_redirect('../../account.php?fail=1');
+
+}
+
 ?>
